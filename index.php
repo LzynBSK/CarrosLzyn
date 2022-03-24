ecd39bbad22174f77e2a84153b5fbd6ef704f594
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2022-03-24 15:09:00 -0300

    aual0324

--- index.php
+++ index.php
@@ -1,26 +1,52 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula PW2</title>
+    <title>Document</title>
 </head>
 <body>
-    <form method="get">
-        <input type="color" name="cor">
-        <input type="text" name="nome" id="">
-        <input type="submit" value="" name="btnEnviar">
-    </form>
-    
     <?php
-        //isset() -> verifica a exitência de uma variável
-        if(isset($_GET['btnEnviar'])){
-            echo '<h1 style="color:'.$_GET['cor'].'">Teset</h1>';            
-        }
-        else{
-            echo '<h1 style="color:black">Teset</h1>';
-        }
+        // class teste{
+        //     public $nome;
+        // }
+        // $nome = "João";
+        // $idade = 10;
+        // $salario = 1111.99;
+        // $casado = false;
+        // $formacao = ['Pedreiro','Pintor','Cozinheiro'];
+        // $objeto = new teste();
+
+        // var_dump($nome,$idade,$salario,$casado,$formacao,$objeto);
+        
+        $usuarios = [
+            'SP' => [
+                'nome'=>'Marcos',
+                'idade'=>18,
+                'formacao' => ['Pedreiro','Pintor','Cozinheiro']
+            ],
+            'user2' => [
+                'nome'=>'Telma',
+                'idade'=>22,
+                'formacao' => ['Engenheira','Arquitéta']
+            ],
+        ];
+
+        var_dump($usuarios);
+
+// ************* Exercício ***************************;
+// Crie uma array com o seguinte conteúdo;
+// 3 estados; 
+//     => dados do estado (nome, população, cidades)
+// Cada estado com no mínimo 3 cidades;
+// 	    => dados das cidades (nome, população, bairros)
+// Cada cidade de cada estado com no mínimo 2 bairros;
+// 	    => dados do bairro (nome, zona (ex: sul), ruas)
+// Cada bairro com no mínimo 2 ruas;
+// 	    => dados da rua (nome, número de casa)
+//****************************************************;
+
     ?>
 </body>
 </html>
